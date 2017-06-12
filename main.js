0986640220c4cadb43b90d9a69255fcafcbb7a20
antweer <trajwani@gmail.com>
2017-06-12 11:13:09 -0500

    Created a class for paddles

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-// pong logic
-
 // setup canvas
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
+
+class drawRectangle{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    var width = 100;
+    var height = 200;
+    var color = 'black';
+    var dy = 5;
+  }
+  
+  draw (y = 0){
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y + y, width, height);
+  }
+}
 
 
 // set canvas width and height to width and height of screen
@@ -15,3 +29,5 @@ window.addEventListener('resize', function() {
   canvas.height = window.innerHeight;
   draw();
 });
+
+//Game logic
