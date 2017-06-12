e8caaf95878c5fd111ba7b50f5761903b9a462c8
JJ <jj.spetseris@yahoo.com>
2017-06-12 11:21:02 -0500

    added Ball class

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-class drawRectangle{
-  constructor(x, y){
+class DrawRectangle {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     var width = 100;
@@ -11,13 +11,41 @@ class drawRectangle{
     var color = 'black';
     var dy = 5;
   }
-  
+
   draw (y = 0){
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y + y, width, height);
   }
 }
 
+// defines Circle object
+class Circle {
+  constructor (x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = 10;
+    this.dy = 10;
+    this.radius = 100;
+    var color = 'black';
+  }
+
+  update() {
+      if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+        this.dx = -this.dx;
+        console.log('GAME OVER');
+      }
+      if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+        this.dy = -this.dy
+      }
+      this.x += this.dx;
+      this.y += this.dy;
+
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+      context.fillStyle = color;
+      context.fill();
+    }
+  }
 
 // set canvas width and height to width and height of screen
 canvas.width = window.innerWidth;
